a8203088220d59c431101a6e3e77915509bf10d2
Alfin Hidayat Darmawan <alfiputra.id@gmail.com>
2022-07-11 15:48:36 +0700

    [INITIAL COMMIT]

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { TambahAlamatComponent } from './tambah-alamat/tambah-alamat.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'buku-alamat';
+  constructor(public dialog: MatDialog) { }
+  buatAlamat() {
+    const dialogRef = this.dialog.open(TambahAlamatComponent, {
+      width: "450px",
+    });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,43 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+// Material Design
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from "@angular/material/button";
+import { MatCardModule } from '@angular/material/card';
+import { MatListModule } from "@angular/material/list";
+import { MatMenuModule } from "@angular/material/menu";
+import { MatDialogModule } from "@angular/material/dialog";
+import { MatFormFieldModule } from "@angular/material/form-field";
+import { MatInputModule } from "@angular/material/input";
+
+// Component
+import { TambahAlamatComponent } from './tambah-alamat/tambah-alamat.component';
+import { DetailAlamatComponent } from './detail-alamat/detail-alamat.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TambahAlamatComponent,
+    DetailAlamatComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+    MatCardModule,
+    MatListModule,
+    MatMenuModule,
+    MatDialogModule,
+    MatFormFieldModule,
+    MatInputModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
